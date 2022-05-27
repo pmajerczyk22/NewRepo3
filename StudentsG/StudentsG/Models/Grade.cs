e719de367e548887f2c770beb1735300a48f91fe
Patryk Majerczyk <patryk.create@gmail.com>
2022-05-27 11:45:28 +0200

    StudentServiceno to zaczynamy

---
+++ StudentsG/StudentsG/Models/Grade.cs
@@ -0,0 +1,8 @@
+﻿namespace StudentsG.Models
+{
+    public class Grade
+    {
+        public decimal Value { get; set; }
+        public int Weight { get; set; }
+    }
+}
---
+++ StudentsG/StudentsG/Services/StudentGradeService.cs
@@ -0,0 +1,18 @@
+﻿using StudentsG.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace StudentsG.Services
+{
+    public class StudentGradeService
+    {
+        public decimal Calculate(IEnumerable<Grade> grades)
+        {
+            var gradesSum = grades.Sum(g => g.Value * g.Weight);
+            return gradesSum / grades.Sum(g => g.Weight);
+        }
+
+    }
+}
